8df6775c2e51f72e20203858414b84245ead3b9d
mocchin0218 <oteketen@gmail.com>
2025-05-31 01:12:59 +0900

    Update script.js

--- car-reservation/script.js
+++ car-reservation/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("today").textContent = today;
 
   document.getElementById("reserveButton").addEventListener("click", () => {
-    const GAS_URL = "https://script.google.com/macros/s/【あなたのデプロイURL】/exec";
+    const GAS_URL = "https://script.google.com/macros/s/【https://script.google.com/macros/s/AKfycby6t_sPU5-eNREsPmlRaDX8Fj9XqSI3kCzgE8zBRw1wXMNIxD0m-Ztc9HRPEwzDfNHN/exec】/exec";
     const carName = today;
 
     fetch(`${GAS_URL}?car=${encodeURIComponent(carName)}`)
